879bb72032ef29e948f4f7d39c6db9d647daac1b
nishant <palln6935@gmail.com>
2020-06-06 15:48:04 +0530

    deleting todos

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -1,3 +1,13 @@
+// Check off specific todos by clicking
 $('li').click(function(){
 	$(this).toggleClass('completed');
+});
+
+// click on X to delete todo
+$('span').click(function(event){
+	//fadeOut the parent
+	$(this).parent().fadeOut(500,function(){ 
+		$(this).remove();	//remove parent from HTML
+	});
+	event.stopPropagation();//to prevent bubbling of events
 });
