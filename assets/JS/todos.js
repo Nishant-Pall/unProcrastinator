76f2cf3b9412a28497e58beb138f8eb1d31e6c65
nishant <palln6935@gmail.com>
2020-06-06 16:22:36 +0530

    adding new ToDos

--- assets/JS/todos.js
+++ assets/JS/todos.js
@@ -1,13 +1,23 @@
 // Check off specific todos by clicking
-$('li').click(function(){
+$('ul').on('click', 'li',function(){
 	$(this).toggleClass('completed');
 });
 
 // click on X to delete todo
-$('span').click(function(event){
+$('ul').on('click', 'span',function(event){
 	//fadeOut the parent
 	$(this).parent().fadeOut(500,function(){ 
 		$(this).remove();	//remove parent from HTML
 	});
 	event.stopPropagation();//to prevent bubbling of events
+});
+
+$("input[type='text'").keypress(function(event){
+	if(event.which === 13){
+		// grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		// create new li and add it to ul
+		$('ul').append("<li><span>X</span> " + todoText + "</li>")
+	}
 });
